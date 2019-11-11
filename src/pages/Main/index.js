bfac790caf4e342e186ccf7b351f541d4d49607d
patrickt26 <plthomaz@gmail.com>
2019-11-11 20:07:00 -0300

    Exibindo repositório

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
@@ -71,7 +72,7 @@ export default class Main extends Component {
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton loading={loading}>
+          <SubmitButton loading={loading ? 1 : 0}>
             {loading ? (
               <FaSpinner color="#FFF" size={14} />
             ) : (
